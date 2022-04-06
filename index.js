9c41b1be9a102d36bfe4da99ddeb83b51336907f
dianabasilio <dianabasilio99@gmail.com>
2022-04-06 06:00:50 -0500

    css, html, js

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function Fibo(){
+    let number2 = parseInt(document.getElementById("inputFibo").value);
+    console.log(number2);
+    
+    let fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (var i = 2; i < number2; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    console.log(fibonacci);
+
+    const container = document.getElementById("fiboContainer");
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "Fibonacci of <b>"+number2+"</b>= "+fibonacci[fibonacci.length - 1];
+    container.appendChild(paragraph);
+
+}
+
+
+function Factorial(){
+    const number1 = parseInt(document.getElementById("inputFactorial").value);
+    console.log(number1);
+
+    let numberToShow = [];
+    let answerFact = 0;
+
+    if (number1 !== 0 && number1 <15 ){
+
+        for (let i = 1; i <= number1; i++){
+            numberToShow.push(i);
+        };
+    
+        answerFact = numberToShow.reduce(function(a, b){return a*b});
+
+    }    
+    else if (number1 >=15){
+        answerFact = "Too long";
+        
+    }else{
+        answerFact = 1;
+    }
+
+    const container = document.getElementById("fiboFactorial");
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = "Factorial of <b>"+number1+"</b>= "+answerFact;
+    container.appendChild(paragraph);
+
+}
+
